e1aff284e90a6020479f907d7580d98b027adf3d
Mike Dorn <mjdorn99@gmail.com>
2025-05-28 20:19:40 -0400

    JavaScript: es6 classes exercise

---
+++ javascript-part-2/es6-classes-exercises.js
@@ -0,0 +1,20 @@
+// #MARK: Exercise 1
+// Create a stack class
+
+// Stack {}
+//   count: 0
+//   __proto__:
+//     constructor: class Stack
+//     count: (...)
+//     peek: f peek()
+//     pop: f pop()
+//     push: f push(obj)
+//     get_count: f count()
+//     __proto__: Object
+
+// peek - shows you whats on top of the stack
+//  - Throws error when stack is empty
+// pop removes the object from the stack and logs it
+//  - Throws error when stack is empty
+// push adds the object to the top of the stack
+// count gets how many are in the stack
