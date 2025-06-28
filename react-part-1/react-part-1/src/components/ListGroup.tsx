7cc82301572698ea716eefe5395839e20ee28f2f
Mike Dorn <mjdorn99@gmail.com>
2025-06-28 16:54:43 -0400

    React: Managing State

--- react-part-1/react-part-1/src/components/ListGroup.tsx
+++ react-part-1/react-part-1/src/components/ListGroup.tsx
@@ -1,7 +1,23 @@
-import type { MouseEvent } from "react";
+import { useState, type MouseEvent } from "react";
 
 function ListGroup() {
   const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  // Variable for determining which input box is selected
+  let selectedIndex = -1;
+  // ^ This variable is local, react is not aware of when it changes, and thus doesn't refresh
+
+  // Add a hook | This returns an array
+  // const exampleHook = useState(-1);
+  // exampleHook[0] | Variable (selectedIndex)
+  // exampleHook[1] | updator function
+
+  // Implementing a hook, its best to do 2 varirables at the same time to seperate them
+  // const [name, setName] = useState(""); | <- Another example
+  const [newSelectedIndex, setSelectedIndex] = useState(-1);
+
+  // Note: Each component has its own state
+  // If we add another <ListGroup /> component, it will be completely seperate from the other instance
 
   // Remember that .map says:
   // - Converts each item, to an item of a different type
@@ -18,13 +34,35 @@ function ListGroup() {
 
   // We know this is "MouseEvent" because we hovered over the original code, since this is just abstracting it
   // This function is an "Event Handler"
-  const handleClick = (event: MouseEvent) => console.log(event);
+  const handleClick = (event: MouseEvent, index: number) => {
+    selectedIndex = index;
+    console.log(event);
+  };
 
   // NEW VERSION OF LIST ITEMS
-  listItems = items.map((item) => (
+  listItems = items.map((item, index) => (
     // Doesn't use handleClick(), function isn't called, only referenced
     // This is because the function isn't called now, its called at runtime
-    <li className="list-group-item" key={item} onClick={handleClick}>
+    <li
+      className={`list-group-item ${selectedIndex === index && "active"}`}
+      key={item}
+      // Doesn't work
+      onClick={(event) => handleClick(event, index)}
+    >
+      {item}
+    </li>
+  ));
+
+  // Working verrsion of list items
+  listItems = items.map((item, index) => (
+    // Doesn't use handleClick(), function isn't called, only referenced
+    // This is because the function isn't called now, its called at runtime
+    <li
+      className={`list-group-item ${newSelectedIndex === index && "active"}`}
+      key={item}
+      // Doesn't work
+      onClick={() => setSelectedIndex(index)}
+    >
       {item}
     </li>
   ));
