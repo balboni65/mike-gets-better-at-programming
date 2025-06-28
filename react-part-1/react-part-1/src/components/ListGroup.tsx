c3a5eb81876670ca350e3645cf98fae2e0ac4af7
Mike Dorn <mjdorn99@gmail.com>
2025-06-27 23:43:47 -0400

    React: Conditional Rendering

--- react-part-1/react-part-1/src/components/ListGroup.tsx
+++ react-part-1/react-part-1/src/components/ListGroup.tsx
@@ -6,6 +6,20 @@ function ListGroup() {
   // Remember to set a unique key for each element with "key="
   let listItems = items.map((item) => <li key={item}>{item}</li>);
 
+  // Conditional Rendering
+  if (items.length === 0)
+    return (
+      <>
+        <h1>Testing Failure!</h1>
+        <p>No item found</p>
+      </>
+    );
+
+  // Conditional Rendering
+  const getMessage = () => {
+    return items.length >= 20 ? <p>Too many items!</p> : null;
+  };
+
   return (
     // Cannot send a second element
     // <h1></h1> | Error, could wrap in a div to fix
@@ -20,6 +34,16 @@ function ListGroup() {
     <>
       {/* Can now add another element */}
       <h1>Test!</h1>
+
+      {/* Conditional Rendering */}
+      {items.length >= 20 ? <p>Too many items!</p> : null}
+      {/* Or instead do this: */}
+      {getMessage()}
+      {/* Or instead do this: */}
+      {items.length >= 20 && <p>Too many items!</p>}
+      {/* In JavaScript, doing "true && value" returns the value */}
+      {/* Therefor we can do a conditional statement and immediately return something, without having an else condition */}
+      {/* If False, nothing happens */}
 
       {/* Now sends the list group, with an array of list items, just have to pass the variable */}
       <ul className="list-group">{listItems}</ul>
