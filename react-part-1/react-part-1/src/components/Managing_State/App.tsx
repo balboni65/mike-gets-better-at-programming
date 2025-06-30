13595ce4da2ca215dcf2ed339430c05dc939e6ea
Mike Dorn <mjdorn99@gmail.com>
2025-06-30 01:26:33 -0400

    React: State Structure

--- react-part-1/react-part-1/src/components/Managing_State/App.tsx
+++ react-part-1/react-part-1/src/components/Managing_State/App.tsx
@@ -2,9 +2,34 @@ import { useState } from "react";
 
 function App() {
   const [isVisible, setVisibility] = useState(false);
-  const [isApproved, setApproved] = useState(true);
   // Note: You can only use hooks at the top level of your component
   // Order matters, you cannot use these in conditional places
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const fullName = firstName + " " + lastName;
+  // Note: you should not declare a "useState" for "fullName", unecessary
+
+  const [isLoading, setLoading] = useState(""); // Monitors weather the page is loading
+  // Note: this does something very different to the first two
+  //  - The first two manage the properties of a person
+  //  - This manages the state of a page, therefore they should be seperated.
+
+  // Take those values, and move them in an object as they are related
+  const [person, setPerson] = useState({ firstName: "", lastName: "" });
+  // Try to keep these at "1 level" and not nest your objects.
+  const [personDeep, setPersonDeep] = useState({
+    firstName: "",
+    lastName: "",
+    // BAD
+    contact: {
+      address: {
+        street: {
+          length: "",
+        },
+      },
+    },
+  });
 
   let count = 0;
 
